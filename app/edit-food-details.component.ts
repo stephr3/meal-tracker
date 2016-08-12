9a7f199a5c68639a23b828338f6e21d7284fc735
Stephanie Gurung <stephr3@gmail.com>
2016-08-12 08:45:44 -0700

    allow user to edit food details, highlight selected food with blue color

---
+++ app/edit-food-details.component.ts
@@ -0,0 +1,15 @@
+import { Component } from 'angular2/core';
+import { Food } from './food.model';
+
+@Component({
+  selector: 'edit-food-details',
+  inputs: ['food'],
+  template: `
+    <input [(ngModel)]="food.name">
+    <input [(ngModel)]="food.calories">
+    <input [(ngModel)]="food.details">
+  `
+})
+export class EditFoodDetailsComponent {
+  public food: Food;
+}
--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,15 +1,27 @@
-import { Component } from 'angular2/core';
+import { Component,EventEmitter } from 'angular2/core';
 import { Food } from './food.model';
 import { FoodComponent } from './food.component';
+import { EditFoodDetailsComponent } from './edit-food-details.component';
 
 @Component({
   selector: 'food-list',
   inputs: ['foodList'],
-  directives: [FoodComponent],
+  directives: [FoodComponent, EditFoodDetailsComponent],
   template: `
-    <food-display *ngFor="#currentFood of foodList" [food]="currentFood"></food-display>
+    <edit-food-details *ngIf="selectedFood" [food]="selectedFood"></edit-food-details>
+    <food-display *ngFor="#currentFood of foodList" [food]="currentFood" (click)="foodClicked(currentFood)" [class.selected]="currentFood === selectedFood"></food-display>
   `
 })
 export class FoodListComponent {
   public foodList: Food[];
+  public onFoodSelect: EventEmitter<Food>;
+  public selectedFood: Food;
+  constructor(){
+    this.onFoodSelect = new EventEmitter();
+  }
+  foodClicked(clickedFood: Food): void{
+    this.selectedFood = clickedFood;
+    this.onFoodSelect.emit(clickedFood);
+    console.log(clickedFood);
+  }
 }
